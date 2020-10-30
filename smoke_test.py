166548cfb0cc24f99167898ea52fc4cb266b315f
gc25 <36566428+gc25@users.noreply.github.com>
2020-10-30 15:49:45 +0530

    Update smoke_test.py

--- smoke_test.py
+++ smoke_test.py
@@ -2,7 +2,7 @@ from shlex import split
 from subprocess import check_call
 from urllib.request import urlopen
 
-command_line = "docker run -d --name db --health-cmd "curl --fail http://localhost:8080/ || exit 1" --health-interval=5s --timeout=3s arungupta/couchbase"
+command_line = "docker run -d --name db --health-cmd 'curl --fail http://localhost:8080/ || exit 1' --health-interval=5s --timeout=3s arungupta/couchbase"
 docker_args = split(command_line)
 
 check_call(docker_args)
