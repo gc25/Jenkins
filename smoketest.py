93b68da84bc76dd8b386b6dbf984864d06ef75c4
gc25 <36566428+gc25@users.noreply.github.com>
2020-10-30 14:56:49 +0530

    Update smoketest.py

--- smoketest.py
+++ smoketest.py
@@ -1,15 +1,15 @@
-import time
+#import time
 from shlex import split
 from subprocess import check_call
 from urllib.request import urlopen
 
-command_line = "docker run --rm --name=smk -p 8181:8080 -p 50350:50000 --user root -v /var/run/docker.sock:/var/run/docker.sock kgrishma/jenkins:v1"
+command_line = "docker run --rm -d --name=smk -p 8181:8080 -p 50350:50000 --user root -v /var/run/docker.sock:/var/run/docker.sock kgrishma/jenkins:v1 sleep 10"
 args = split(command_line)
 
 check_call(args)
 # Wait for the server to start. A better implementation
 # would poll in a loop:
-time.sleep(10)
+# time.sleep(10)
 # Check if the server started (it'll throw an exception
 # if not):
 try:
