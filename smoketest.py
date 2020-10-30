0dada7ab0d7f5f16d0a86d829af43625a1421b3e
gc25 <36566428+gc25@users.noreply.github.com>
2020-10-30 15:19:29 +0530

    Update smoketest.py

--- smoketest.py
+++ smoketest.py
@@ -3,10 +3,15 @@ from shlex import split
 from subprocess import check_call
 from urllib.request import urlopen
 
-command_line = "docker run --rm -d --name=smk -p 8181:8080 -p 50350:50000 --user root kgrishma/jenkins:v1 sleep 10"
-args = split(command_line)
+kill_command = "lsof -t -i tcp:8181 | xargs kill -9"
+kill_args = split(kill_command)
 
-check_call(args)
+check_call(kill_args)
+
+command_line = "docker run --rm -d --name=smk -p 8181:8080 -p 50350:50000 --user root kgrishma/jenkins:v1 sleep 10"
+docker_args = split(command_line)
+
+check_call(docker_args)
 # Wait for the server to start. A better implementation
 # would poll in a loop:
 # time.sleep(10)
