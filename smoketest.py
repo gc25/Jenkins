14ead35edb577c1b165433ccc626c6efdb8eeb8a
gc25 <36566428+gc25@users.noreply.github.com>
2020-10-30 14:12:11 +0530

    Update smoketest.py

--- smoketest.py
+++ smoketest.py
@@ -1,19 +1,11 @@
 import time
 import sys
 
-from subprocess import check_call
 from urllib.request import urlopen
 
-
-check_call(
-    "docker run --rm -d --name=smk -p 8181:8080 -p 50340:50000 --user root -v /var/run/docker.sock:/var/run/docker.sock kgrishma/my-image:30".split()
-)
-# Wait for the server to start. A better implementation
-# would poll in a loop:
-time.sleep(60)
 # Check if the server started (it'll throw an exception
 # if not):
 try:
     urlopen("http://localhost:8181").read()
 finally:
-    check_call("docker kill smk".split())
+    print("Jenkins server is accessible")
