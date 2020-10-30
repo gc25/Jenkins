d440f33bff87e567261512665a134ee1746fe617
gc25 <36566428+gc25@users.noreply.github.com>
2020-10-30 10:14:31 +0530

    Create smoketest.py

---
+++ smoketest.py
@@ -0,0 +1,16 @@
+import time
+from subprocess import check_call
+from urllib.request import urlopen
+
+check_call(
+     “docker run --rm --name=smk -p 8080:8080 -p 50000:50000 --user root -v /var/run/docker.sock:/var/run/docker.sock kgrishma/jenkins:v1”.split()
+)
+# Wait for the server to start. A better implementation
+# would poll in a loop:
+time.sleep(60)
+# Check if the server started (it'll throw an exception
+# if not):
+try:
+    urlopen("http://localhost:8080").read()
+finally:
+    check_call("docker kill smk".split())
